fe26b8ae8a0e82fa5a6f1fa74526d09c176eb3fb
Andres G. Aragoneses <knocte@gmail.com>
2019-12-23 17:37:41 +0800

    hello-world.cs: test HTTP connection within the app

--- hello-world/hello-world.cs
+++ hello-world/hello-world.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Net;
 
 public static class Program
 {
     public static void Main()
     {
         Console.WriteLine("Hello, world!");
+
+        using (var wc = new WebClient()) {
+            // ip address is obtained via `dig +short httpvshttps.com`
+            var content = wc.DownloadString("http://45.33.7.16/robots.txt");
+            Console.WriteLine(content);
+        }
+
+        Console.WriteLine("Bye, world!");
     }
 }
